9f4a51b631f2dcd6d127afb6bf15e67e7b16f32f
erezarbell <38767854+ErezArbell@users.noreply.github.com>
2023-11-06 20:26:53 +0200

    First: circles in colors

---
+++ 1.py
@@ -0,0 +1,21 @@
+import turtle
+
+colors = ["red", "blue", "purple", "orange", "yellow", "green"]
+
+t = turtle.Turtle()
+t.pencolor("black")
+t.fillcolor("orange")
+# turtle.bgcolor("blue")
+t.pensize(2)
+t.speed(10)
+
+for i in range(10):
+  t.pencolor(colors[i % len(colors)])
+  t.circle(20*(10-i))
+  t.penup()
+  t.left(90)
+  t.forward(20)
+  t.right(90)
+  t.pendown()
+
+turtle.mainloop()
